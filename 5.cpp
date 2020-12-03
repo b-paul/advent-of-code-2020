1f7ab3bb2b26e8f09099fef0875ed419cdd2bb61
Benjamin Paul <bpaul848@gmail.com>
2020-12-03 17:44:06 +1000

    Puzzle 5!!!!!

---
+++ 5.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define FOR(i,a) for (i = 0; i < (a); i++)
+
+#define SQ(a) (a)*(a)
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+// Real modulo that fixes negatives
+#define MOD(a,b) (((a)%(b))<0?(((a)%(b))+(b)):((a)%(b)))
+
+void hi() {
+    vector<uint32_t> map(323,0);
+    // one row is 31 wide, so 32 bits is perfect!
+
+    int i, j, n;
+    string l;
+    FOR (i, 323) {
+        cin >> l;
+        n=0;
+        FOR(j, 31) {
+            if (l[j] == '#')
+                n |= 1 << j;
+        }
+        map[i] = n;
+    }
+
+    j=0,n=0;
+    FOR (i, 323) {
+        if (map[i] & (1 << (j%31)))
+            n++;
+        j+=3;
+    }
+
+    cout << n << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t=1;
+    //cin >> t;
+
+    for (int i = 0; i < t; i++)
+        hi();
+
+    return 0;
+}
